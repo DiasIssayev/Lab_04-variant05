084516bd7de63482a48f47be40c01520962725ca
DiasIssayev <diasisaev68@gmail.com>
2026-02-19 11:21:55 +0500

    Public github repo added

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,15 +2,17 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"Lab_04-variant05/pkg/powerbill"
 
 	"github.com/google/uuid"
+	"github.com/olekukonko/tablewriter"
 )
 
 func main() {
 
-	// UUID из внешнего пакета GitHub
+	// Генерация ID отчёта
 	id := uuid.New()
 	fmt.Printf("Report ID: %s\n\n", id)
 
@@ -19,29 +21,46 @@ func main() {
 	tariff := 0.18
 	discount := 10.0
 
+	// Расчёт потребления
 	kwh, err := powerbill.Consumption(prev, curr)
 	if err != nil {
 		fmt.Println("Error:", err)
 		return
 	}
 
+	// Расчёт стоимости
 	cost, err := powerbill.EnergyCost(kwh, tariff)
 	if err != nil {
 		fmt.Println("Error:", err)
 		return
 	}
 
+	// Применение скидки
 	err = powerbill.ApplyDiscount(&cost, discount)
 	if err != nil {
 		fmt.Println("Error:", err)
 		return
 	}
 
+	// Формирование текстового отчёта
 	report, err := powerbill.FormatEnergyReport("Dias", kwh, cost)
 	if err != nil {
 		fmt.Println("Error:", err)
 		return
 	}
 
-	fmt.Printf("%s\n", report)
+	fmt.Println(report)
+	fmt.Println()
+
+	// Таблица
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetHeader([]string{"Owner", "Consumption (kWh)", "Cost"})
+
+	table.Append([]string{
+		"Dias",
+		fmt.Sprintf("%.2f", kwh),
+		fmt.Sprintf("%.2f", cost),
+	})
+
+	table.Render()
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,13 @@ module Lab_04-variant05
 
 go 1.25
 
-require github.com/google/uuid v1.6.0
+require (
+	github.com/google/uuid v1.6.0
+	github.com/olekukonko/tablewriter v0.0.5
+)
+
+require (
+	github.com/clipperhouse/stringish v0.1.1 // indirect
+	github.com/clipperhouse/uax29/v2 v2.3.0 // indirect
+	github.com/mattn/go-runewidth v0.0.19 // indirect
+)
